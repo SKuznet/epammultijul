cf680650c183565e59a31b518752caf76a252f45
Pavel Ezhkov <pavel_ezhkov@epam.com>
2019-07-16 22:34:29 +0300

    Adds traffic light

---
+++ src/main/java/com/epam/multi/lesson1/homework1/Main.java
@@ -0,0 +1,12 @@
+package com.epam.multi.lesson1.homework1;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] seconds = new int[]{11, 4, 9};
+        for (int i = 0; i < seconds.length; i++) {
+            TrafficLight trafficLight = new TrafficLight(seconds[i]);
+            Thread thread = new Thread(trafficLight);
+            thread.start();
+        }
+    }
+}
---
+++ src/test/com/epam/multi/lesson1/homework1/TrafficLightTest.java
@@ -0,0 +1,35 @@
+package com.epam.multi.lesson1.homework1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TrafficLightTest {
+
+    private TrafficLight trafficLight;
+
+
+    @AfterEach
+    void tearDown() {
+        trafficLight = null;
+    }
+
+    @Test
+    void runRed() {
+        trafficLight = new TrafficLight(1);
+        assertEquals(trafficLight.showColor(), "Красный");
+    }
+
+    @Test
+    void runYellow() {
+        trafficLight = new TrafficLight(4);
+        assertEquals(trafficLight.showColor(), "Желтый");
+    }
+
+    @Test
+    void runGreen() {
+        trafficLight = new TrafficLight(9);
+        assertEquals(trafficLight.showColor(), "Зеленый");
+    }
+}
