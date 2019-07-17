55b9a1bc31469ee5660d9d402ecded8edb665f49
Maksim <Maksim_Golota@epam.com>
2019-07-17 15:50:43 +0300

    creates TrafficLight

--- src/main/java/hw1/Main.java
+++ src/main/java/hw1/Main.java
@@ -8,24 +8,67 @@ import java.util.List;
 
 public class Main {
 
-    public static List<Integer> minutes;
+    private static List<Integer> schedule = new ArrayList<>();
+    private static int size;
+    private static int count = 0;
+    private static TrafficLight trafficLight;
 
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            minutes = new ArrayList<>();
+
+        consoleReader();
+        size = schedule.size();
+        trafficLight = new TrafficLight(2, 3, 4);
+        Thread thread_1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (count<size) {
+                    showLight();
+                    count++;
+                }
+            }
+        });
+        Thread thread_2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (count<size) {
+                    showLight();
+                    count++;
+                }
+            }
+        });
+        Thread thread_3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (count<size) {
+                    showLight();
+                    count++;
+                }
+            }
+        });
+
+        thread_1.start();
+        thread_2.start();
+        thread_3.start();
+
+
+    }
+
+
+    public static void consoleReader() {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             while (true) {
-                String minute = reader.readLine();
-                if (minute.isEmpty()) {
+                String s = bufferedReader.readLine();
+                if (s.isEmpty()) {
                     break;
                 } else {
-                    minutes.add(Integer.parseInt(minute));
+                    schedule.add(Integer.parseInt(s));
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        TrafficLights trafficLights = new TrafficLights(2,3,4);
-        trafficLights.getLight(minutes);
+    }
+    public static synchronized void showLight() {
+        trafficLight.findLight(schedule.get(count));
     }
 }
