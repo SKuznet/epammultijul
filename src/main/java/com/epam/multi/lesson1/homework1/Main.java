31219e214aaf746694ff8709d5bc2409e1c16c2a
Vladimir <floydvladimir@gmail.com>
2019-07-17 22:03:30 +0300

    1

--- src/main/java/com/epam/multi/lesson1/homework1/Main.java
+++ src/main/java/com/epam/multi/lesson1/homework1/Main.java
@@ -6,49 +6,5 @@ import java.io.InputStreamReader;
 import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args) {
-        final TrafficLight trafficLight = new TrafficLight();
-        int[][] inputMinutes = new int[3][3];
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        System.out.println("Input time moments");
-        try {
-            for (int i = 0; i < 3; i++) {
-                System.out.println("Input time for thread " + i);
-                for (int j = 0; j < 3; j++) {
-                    inputMinutes[i][j] = Integer.parseInt(br.readLine());
-                }
-            }
-        }
-        catch (IOException ex) {
-        }
 
-        final TrafficLightCheck check0 = new TrafficLightCheck(inputMinutes[0]);
-        final TrafficLightCheck check1 = new TrafficLightCheck(inputMinutes[1]);
-        final TrafficLightCheck check2 = new TrafficLightCheck(inputMinutes[2]);
-        Thread thread0 = new Thread(new Runnable() {
-            public void run() {
-                for(int i=0;i<3;i++) {
-                    trafficLight.checkLight(check0.getMinutes()[i]);
-                }
-            }
-        });
-        Thread thread1 = new Thread(new Runnable() {
-            public void run() {
-                for(int i=0;i<3;i++) {
-                    trafficLight.checkLight(check1.getMinutes()[i]);
-                }
-            }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            public void run() {
-                for(int i=0;i<3;i++) {
-                    trafficLight.checkLight(check2.getMinutes()[i]);
-                }
-            }
-        });
-        thread0.start();
-        thread1.start();
-        thread2.start();
-    }
 }
