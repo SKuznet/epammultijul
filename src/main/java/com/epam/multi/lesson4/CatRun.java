9c2272587045c2e55c497aa5b406482742c47e5a
skuznet <azon.sk@gmail.com>
2019-07-22 17:15:28 +0300

    add l4

---
+++ src/main/java/com/epam/multi/lesson4/CatRun.java
@@ -0,0 +1,48 @@
+package com.epam.multi.lesson4;
+
+import java.util.concurrent.TimeUnit;
+
+public class CatRun {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        System.err.println(Thread.currentThread().getName() + " " + i);
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.err.println();
+                }
+            }
+        });
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (int i = 0; i < 10; i++) {
+                    Thread.currentThread().setName("Joiner");
+                    System.err.println(Thread.currentThread().getName() + " " + i);
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.err.println();
+                }
+            }
+        });
+
+        thread.start();
+        thread1.start();
+
+    }
+}
