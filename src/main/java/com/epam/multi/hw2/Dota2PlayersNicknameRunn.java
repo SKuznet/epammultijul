6433513c7f6cc927d9451a374a3a9cadfd554772
soignera <askasoignera@gmail.com>
2019-07-22 13:27:27 +0300

    add 2 homework Callable and Future

--- src/main/java/com/epam/multi/hw2/Dota2PlayersNicknameRunn.java
+++ src/main/java/com/epam/multi/hw2/Dota2PlayersNicknameRunn.java
@@ -3,7 +3,7 @@ package com.epam.multi.hw2;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Dota2PlayersNickname implements Runnable {
+public class Dota2PlayersNicknameRunn implements Runnable {
     private int count = 0;
     private volatile double d;
     private int priority;
@@ -13,7 +13,7 @@ public class Dota2PlayersNickname implements Runnable {
         return "Dota2player nickname :" + Thread.currentThread() + ":" + " priority=" + priority;
     }
 
-    public Dota2PlayersNickname(int priority) {
+    public Dota2PlayersNicknameRunn(int priority) {
         this.priority = priority;
     }
 
@@ -35,10 +35,10 @@ public class Dota2PlayersNickname implements Runnable {
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.execute(new Dota2PlayersNickname(Thread.MAX_PRIORITY));
+        executorService.execute(new Dota2PlayersNicknameRunn(Thread.MAX_PRIORITY));
 
         for (int i = 0; i < 5; i++) {
-            executorService.execute(new Dota2PlayersNickname(Thread.MIN_PRIORITY));
+            executorService.execute(new Dota2PlayersNicknameRunn(Thread.MIN_PRIORITY));
         }
 
         executorService.shutdown();
