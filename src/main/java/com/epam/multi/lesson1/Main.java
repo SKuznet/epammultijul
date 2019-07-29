9c04392767f8488836a7b6adbb1a123d12d391c6
Maksim <Maksim_Golota@epam.com>
2019-07-29 11:33:11 +0300

    makes task 2

--- src/main/java/com/epam/multi/lesson1/Main.java
+++ src/main/java/com/epam/multi/lesson1/Main.java
@@ -1,33 +1,8 @@
 package main.java.com.epam.multi.lesson1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Main {
 
-    List<Integer> minutes = new ArrayList<>();
-    static int count = 3;
 
-    public static void main(String[] args) {
-
-        Main main = new Main();
-        main.ConsoleReader();
-        TrafficLight trafficLight = new TrafficLight(2, 3, 4);
-        ExecutorService service = Executors.newFixedThreadPool(count);
-        for (int i = 0; i < count; i++) {
-            service.execute(new Runnable() {
-                @Override
-                public void run() {
-
-                }
-            });
-        }
 
 
         /*Thread thread = new Thread(new Runnable() {
@@ -54,20 +29,5 @@ public class Main {
             }
         });
     }*/
-    }
 
-    public void ConsoleReader() {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            while (true) {
-                String s = bufferedReader.readLine();
-                if (s == null) {
-                    break;
-                } else {
-                    minutes.add(Integer.parseInt(s));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
