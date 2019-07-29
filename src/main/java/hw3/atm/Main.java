6b9846e87e519e44a7fb40371ac870d447fd5943
Maksim <Maksim_Golota@epam.com>
2019-07-29 12:38:31 +0300

    does homework4

---
+++ src/main/java/hw3/atm/Main.java
@@ -0,0 +1,14 @@
+package main.java.hm3.atm;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cardholder ch1 = new Cardholder("Bob");
+        Cardholder ch2 = new Cardholder("John");
+        Cardholder ch3 = new Cardholder("Jack");
+        ch1.start();
+        ch2.start();
+        ch3.start();
+    }
+}
