a314baca53fdf70b511780f9558bcad65fb33218
Karim Magomedov <ma9omedov@gmail.com>
2019-08-02 10:33:22 +0300

    add semaphore task

---
+++ src/main/java/com/epam/multi/lesson6/hw4/GameRoom.java
@@ -0,0 +1,35 @@
+package com.epam.multi.lesson6.hw4;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class GameRoom extends Thread {
+    private Semaphore semaphore;
+    private int gamePlayingCount;
+    private int gamerId;
+
+    public GameRoom(Semaphore semaphore, int gamerId) {
+        this.semaphore = semaphore;
+        this.gamerId = gamerId;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (gamePlayingCount < 3) {
+                semaphore.acquire();
+                System.out.println("Gamer " + gamerId + " play in the game");
+                TimeUnit.MILLISECONDS.sleep(500);
+                gamePlayingCount++;
+                System.out.println("Gamer " + gamerId + " play " + gamePlayingCount);
+                System.out.println("Gamer " + gamerId + " leave the room");
+                semaphore.release();
+
+                TimeUnit.MILLISECONDS.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Gamer " + gamerId + " is crying");
+        }
+
+    }
+}
---
+++ src/main/java/com/epam/multi/lesson6/hw4/GameRoomWithSemaphoreApp.java
@@ -0,0 +1,15 @@
+package com.epam.multi.lesson6.hw4;
+
+import com.epam.multi.lesson6.hw4.GameRoom;
+
+import java.util.concurrent.Semaphore;
+
+public class GameRoomWithSemaphoreApp {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        for (int i = 0; i < 6; i++) {
+            new GameRoom(semaphore, i).start();
+
+        }
+    }
+}
