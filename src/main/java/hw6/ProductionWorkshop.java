7d20c43717699b516edc38a4e0f9d147cce766c1
Aleksandr Lutkov <Aleksandr_Lutkov@epam.com>
2019-08-02 08:11:37 +0300

    Adds hw6

--- src/main/java/hw6/ProductionWorkshop.java
+++ src/main/java/hw6/ProductionWorkshop.java
@@ -3,22 +3,38 @@ package hw6;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Exchanger;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProductionWorkshop implements Runnable {
 
     private Semaphore semaphore;
     private CountDownLatch countDownLatch;
-    private Exchanger<AtomicInteger> exchanger;
+    private Item item;
 
-    public ProductionWorkshop(Semaphore semaphore, CountDownLatch countDownLatch, Exchanger<AtomicInteger> exchanger) {
+    public ProductionWorkshop(Semaphore semaphore, CountDownLatch countDownLatch, Item item) {
         this.semaphore = semaphore;
         this.countDownLatch = countDownLatch;
-        this.exchanger = exchanger;
+        this.item = item;
     }
 
     @Override
     public void run() {
+
+        try {
+            semaphore.acquire();
+            TimeUnit.MILLISECONDS.sleep(1000);
+
+
+
+
+
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
